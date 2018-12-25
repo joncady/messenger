75deb89c024f7a7efbacb412785910dd7ef47f8c
Connie Wang <2conniewang@gmail.com>
2018-12-24 19:31:23 -0800

    fix merge errors

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import UserSignIn from "./components/UserSignIn";
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import ChatsList from './components/ChatsList.js';
-
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
+import "firebase/firestore";
+import MessageArea from './components/MessageArea';
+import { Row, Col } from 'reactstrap';
 
 // Initialize Cloud Firestore through Firebase
-var db = firebase.firestore();
 
-// Disable deprecated features
-db.settings({
-  timestampsInSnapshots: true
-});  
+
 
 let chatList = [{
 	reciever: "sonic the hedgehog",
@@ -30,7 +25,7 @@ let chatList = [{
 	time: "4200000"
 }];
 
-class App extends Component {
+export default class App extends Component {
 
 	constructor() {
 		super();
@@ -49,46 +44,34 @@ class App extends Component {
 		firebase.auth().signOut();
 	}
 
-		// add temp data to
-		// var ref = db.collection("conversations");
+	// add temp data to
+	// var ref = db.collection("conversations");
 
-		// ref.doc().set({
-		// 	image: "hi.jpg",
-		// 	users: ["KQw0dyz2ZsjzGAhF8BKM", "TMimSglHAAc9POJMVjTX"] });
-		// ref.doc().set({
-		// 	image: "hello.jpg",
-		// 	users: ["TMimSglHAAc9POJMVjTX", "fakeuser222"] });
+	// ref.doc().set({
+	// 	image: "hi.jpg",
+	// 	users: ["KQw0dyz2ZsjzGAhF8BKM", "TMimSglHAAc9POJMVjTX"] });
+	// ref.doc().set({
+	// 	image: "hello.jpg",
+	// 	users: ["TMimSglHAAc9POJMVjTX", "fakeuser222"] });
 
 	getRecipients(list, user) {
 		let res = "";
-		for(let i = 0; i < list.length; i++) {
-			if(list[i] !== user) {
+		for (let i = 0; i < list.length; i++) {
+			if (list[i] !== user) {
 				res += list[i] + " ";
 			}
 		}
 		return res.trim();
 	}
 
-	render() {
-		return (
-			<div className="App">
-				{this.state.user ?
-					<div>
-						<h1>{this.state.user.displayName}</h1>
-						<button onClick={this.logOut}>Log Out</button>
-						<ChatsList chats={this.state.chats}></ChatsList>
-					</div> :
-					<div>
-						<UserSignUp></UserSignUp>
-						<UserSignIn></UserSignIn>
-					</div>
-				}
-			</div>
-		);
-	}
-
 	componentDidMount() {
-	    this.authUnregFunc = firebase.auth().onAuthStateChanged((firebaseUser) => {
+		var db = firebase.firestore();
+		// // Disable deprecated features
+		db.settings({
+			timestampsInSnapshots: true
+		});
+
+		this.authUnregFunc = firebase.auth().onAuthStateChanged((firebaseUser) => {
 			if (firebaseUser) {
 				this.setState({
 					user: firebaseUser
@@ -99,7 +82,7 @@ class App extends Component {
 				});
 			}
 		});
-		
+
 		let user = "KQw0dyz2ZsjzGAhF8BKM";
 
 		db.collection("conversations").where("users", "array-contains", user).get().then((querySnapshot) => {
@@ -110,14 +93,37 @@ class App extends Component {
 					lastMessage: "hey nerd",
 					time: "201230120"
 				};
-				
+
 				let newChats = this.state.chats.concat(chat);
-				this.setState({chats : newChats});
+				this.setState({ chats: newChats });
 				console.log(this.state.chats);
 			});
 		});
 	}
 
-}
+	render() {
+		return (
+			<div className="App">
+				{this.state.user ?
+					<div>
+						<h1>{this.state.user.displayName}</h1>
+						<button onClick={this.logOut}>Log Out</button>
+						<Row>
+							<Col>
+								<ChatsList chats={this.state.chats}></ChatsList>
+							</Col>
+							<Col>
+								<MessageArea></MessageArea>
+							</Col>
+						</Row>
+					</div> :
+					<div>
+						<UserSignUp></UserSignUp>
+						<UserSignIn></UserSignIn>
+					</div>
+				}
+			</div>
+		);
+	}
 
-export default App;
+}
